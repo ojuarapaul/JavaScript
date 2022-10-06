e7eb11b2c76d9bd3f701f8b4a4458511c67aab4f
Paulo Fagundes Araujo <ojuarapaul@gmail.com>
2022-10-06 13:41:50 -0700

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,31 +1,31 @@
 function getFibonacci(n) {
-    n = n*1; //transforms string in number 
-    
+    n = n * 1; //transforms string in number 
+
     if (n === 1) {
         alert("Fibonacci sequence = [0]");
-        
+
         return fibonacci = [0];
-        
+
     } else if (n === 0) {
         alert("Sequence does not exist");
-        
+
         return "End of program";
-        
+
     } else {
-        fibonacci = [0,1];
-        
-    for (var x = 1; x < (n - 1); x++) {
-        var lastNumber = (fibonacci.at(x)); //gets last number of sequence
-        var prevNumber = (fibonacci.at(x - 1)); //gets previous number of sequence
-        var newNumb = (lastNumber + prevNumber); //adds the two numbers to get the next number of sequence
-        fibonacci.push(newNumb);
+        var fibonacci = [0, 1];
+
+        for (var x = 1; x < (n - 1); x++) {
+            var lastNumber = (fibonacci.at(x)); //gets last number of sequence
+            var prevNumber = (fibonacci.at(x - 1)); //gets previous number of sequence
+            var newNumb = (lastNumber + prevNumber); //adds the two numbers to get the next number of sequence
+            fibonacci.push(newNumb);
+        }
     }
-           }
-    
+
     alert("Fibonacci sequence is: " + fibonacci); //show to alert box
     return fibonacci; //show to console
-    
+
 }
 
-var n = prompt("Type length of Fibonacci Sequence "); 
+var n = prompt("Type length of Fibonacci Sequence ");
 getFibonacci(n);
