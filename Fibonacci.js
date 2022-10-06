ee9f2e65faa710818cfeb70126687e2cd1b8a178
Paulo Fagundes Araujo <ojuarapaul@gmail.com>
2022-10-06 12:49:50 -0700

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -5,7 +5,7 @@ function getFibonacci(n) {
         var lastNumber = (fibonacci.at(x)); //gets last number of sequence
         var prevNumber = (fibonacci.at(x - 1)); //gets previous number of sequence
         var newNumb = (lastNumber + prevNumber); //adds the two numbers to get the next number of sequence
-        fibonacci.push(newNumb);
+        fibonacci.push(newNumb); //inserts the last number found in the end of the array
     }
     alert(fibonacci); //show to alert box
     return fibonacci; //show to console
